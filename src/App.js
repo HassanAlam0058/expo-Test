b725004e6409da7b9449d0291ac98cd65d6552a2
Hassan <md.hassanbb123@gmail.com>
2022-08-28 22:26:14 +0530

    expoTest_BottamTabNavigater

---
+++ src/App.js
@@ -0,0 +1,136 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//       <div className="text-red-800">hassan</div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+export default function App() {
+  return (
+    <>
+
+
+
+
+{/* 
+<header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <svg xmlns="https://www.picxy.com/photo/754090" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">Tailblocks</span>
+    </a>
+    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900">First Link</a>
+      <a class="mr-5 hover:text-gray-900">Second Link</a>
+      <a class="mr-5 hover:text-gray-900">Third Link</a>
+      <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+    </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+</header>
+
+
+
+
+
+
+    
+    <h1 className=" text-blue-900 font-bold  hover:font-thin">
+      
+      Hello world!
+    </h1>
+
+
+    <div class="hover:shadow-lg my-5 bg-gray-800  rounded-lg px-6 py-8 ring-1 ring-gray-900/5 shadow-xl">
+  <div>
+    <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+    </span>
+  </div>
+  <h3 class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
+  <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm">
+    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+  </p>
+</div>
+
+
+
+
+
+
+
+
+<div class=" grid gap-4 grid-cols-2 p-2">
+  <div className=" my-2 bg-black inline-flex items-center   rounded-lg text-white hover:shadow-lg ">01</div>
+  <div className="my-2 bg-black rounded-lg text-white hover:shadow-mg">02</div>
+  <div className="my-2 bg-black rounded-lg text-white hover:shadow-sm red">03</div>
+  <div className="my-2 bg-black rounded-lg text-white hover:shadow-lg">04</div>
+</div> */}
+
+
+
+
+
+
+<div class= "mt-20 ">
+
+  <h1 class="flex justify-center ">   
+    <p class="text-sky-400 text-2xl">G</p> 
+    <p  class="text-red-700 text-2xl">o</p>   
+    <p class="text-yellow-500 text-2xl" >o</p> 
+    <p class="text-sky-600 text-2xl">g</p>  
+    <p class="text-green-600 text-2xl">l</p>    
+    <p class="text-red-700 text-2xl">e</p>    
+  </h1>
+
+
+
+  <div class="flex justify-center">
+    
+    <input type="input" class="border-2 border-rose-400 rounded-full my-5 wy-1000" 
+        /> 
+  </div>
+  <img src="div.png"></img>
+
+
+
+<div class="flex justify-center">
+  <button type="search" class="bg-gray-300 rounded-lg mr-4 text-sm hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
+    Google Search</button>
+
+  <button class="bg-gray-300 rounded-lg text-sm hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">i'm filling lucky
+  </button>
+  </div>
+  </div>
+
+
+    </>
+  )
+}
